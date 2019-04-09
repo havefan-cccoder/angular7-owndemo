bfa19da9d87167b6df7a4add4ccb58fe36d279bc
zmj <1802941855@qq.com>
2019-04-09 11:23:28 +0800

    接口

--- src/app/user/comps/login/login.component.ts
+++ src/app/user/comps/login/login.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../service/user.service';
+import { HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: "Bearer " + localStorage.getItem("token")
+  })
+};
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+  data: any[];
 
-  constructor() { }
+  login(): void {
+    this.userService.getData().subscribe(data => {localStorage.setItem("token", data.token); });
+  }
+
+  getMeg(): void {
+    this.userService.getTopic(httpOptions).subscribe(data =>{ this.data = data;})
+  }
+
+  constructor( private userService: UserService) { }
 
   ngOnInit() {
   }
--- src/app/user/service/user.service.ts
+++ src/app/user/service/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
+const user = {
+  username: 'testjj',
+  password: '96E79218965EB72C92A549DD5A330112'
+};
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private dataUrl = '/master/login';
+  private topicUrl = '/master/lightModel/baseInformation/getWeidu';
+
+  getData(): Observable<any> {
+    return this.http.post<any>(this.dataUrl, user);
+  }
+
+  getTopic(a: any): Observable<any> {
+    return this.http.get<any>(this.topicUrl, a);
+  }
+  constructor(private http: HttpClient) { }
 }
