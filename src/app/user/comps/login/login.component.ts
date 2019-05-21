12ce6a400973bee260ff164b9f870499dc35399c
zhumingjie <zmj@enn.com>
2019-05-21 17:37:21 +0800

    添加登录成功后的跳转

--- src/app/user/comps/login/login.component.ts
+++ src/app/user/comps/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from '../../service/user.service';
 import { from } from 'rxjs';
 import { Md5 } from "ts-md5";
@@ -19,15 +20,16 @@ export class LoginComponent implements OnInit {
     password: null
   }; // 这里是定义变量（属性）
   login(): void {
-    this.user.password = Md5.hashStr(this.user.password);
-    this.userService.login(this.user).subscribe(data => {localStorage.setItem("token", data.token); });
+    const user = Object.assign({}, this.user);
+    user.password = Md5.hashStr(this.user.password);
+    this.userService.login(user).subscribe(data => {if(data.token){localStorage.setItem("token", data.token); this.router.navigate(['quickmodel']);} });
   }
 
   getMeg(): void {
     this.userService.getWeidu().subscribe(data => { this.data = data; });
   }
 
-  constructor( private userService: UserService) { }
+  constructor( private userService: UserService, public router: Router) { }
 
   ngOnInit() {
   }
